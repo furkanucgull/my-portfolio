93a0963714b68764a0cd7ca28ee4f8c6f34cd0e2
furkanucgull <furkanucgull@gmail.com>
2025-03-10 18:19:09 +0300

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./sections/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
+
+function App() {
+  return (
+    <>
+      <main className="max-w-7xl mx-auto">
+        <Navbar />
+        <Hero />
+      </main>
+    </>
+  );
+}
+
+export default App;
