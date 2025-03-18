ace4235c628e01c3f016bcf1dfdfc35b22f56bba
furkanucgull <furkanucgull@gmail.com>
2025-03-18 21:27:28 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -294,14 +294,5 @@ export const workExperiences = [
     icon: "/assets/birlesim.png",
     animation: "clapping",
   },
-  // {
-  //   id: 3,
-  //   name: "Notion",
-  //   pos: "Junior Web Developer",
-  //   duration: "2019 - 2020",
-  //   title:
-  //     "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
-  //   icon: "/assets/notion.svg",
-  //   animation: "salute",
-  // },
+
 ];
